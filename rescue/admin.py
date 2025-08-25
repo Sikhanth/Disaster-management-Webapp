c281de802ae0d46f0ddd20d8fc7d7aa903b82870
Sikhanth SV <sikhanthsv@gmail.com>
2025-08-25 12:46:33 +0530

    added admin module

--- rescue/admin.py
+++ rescue/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Guardian, FamilyMember
 
-# Register your models here.
+@admin.register(Guardian)
+class GuardianAdmin(admin.ModelAdmin):
+    list_display = ("name", "village", "mobile_number")
+    search_fields = ("name", "village", "mobile_number")
+    list_filter = ("village",)
+
+@admin.register(FamilyMember)
+class FamilyMemberAdmin(admin.ModelAdmin):
+    list_display = ("name", "mobile_number", "is_guardian", "guardian", "medical_condition")
+    list_filter = ("is_guardian", "guardian__village")
+    search_fields = ("name", "mobile_number", "medical_condition")
